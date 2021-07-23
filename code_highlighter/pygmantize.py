4d25d0492451a758f4d1dae8bcd05933834a73f3
Alissa Huskey <alissa.huskey@gmail.com>
2021-07-23 15:57:01 -0600

    refactor dynamic class definition
    Use type() and typing.new_class() to dynamically define lexer class
    instead of embedding HighlightedCodeLexer in the make_lexer() function.

--- code_highlighter/pygmantize.py
+++ code_highlighter/pygmantize.py
@@ -2,6 +2,7 @@
 
 from dataclasses import dataclass
 import sys
+from types import new_class
 
 from pygments.lexer import inherit, using, bygroups, Lexer
 from pygments.token import *
@@ -20,6 +21,8 @@ class Cfg:
 
 DEFAULT = Cfg()
 
+class ArgumentError(BaseException): ...
+
 class HighlightedStyle(SphinxStyle):
     """A pygments style adding a an Highlight subtype to all existing tokens,
        mapped to styles the same style with a background color appended.
@@ -32,19 +35,67 @@ class HighlightedStyle(SphinxStyle):
         if Generic is not t
     })
 
+def __meta_repr__(cls):
+    """A cleaner class repr"""
+    return f"<class {cls.__name__}>"
 
-class ArgumentError(BaseException): ...
+class HighlightedCodeLexer():
+    """A pygments lexer base class to that adds a Highlight subtype to all tokens found
+       between the marker text (!!!) and let the parent class lex everything else."""
+
+    def add_highlight(lexer, match, ctx=None):
+        """Add a Highlight subtype to tokens between markers while stripping the
+           markers themselves.
+
+           Expects 4 match groups: marker, highlighted content, marker, rest of line
+        """
+
+        # make a callback function that assigns Token.Marker to the markers, run the
+        # enclosed text through the parent lexer, then run the rest of the line back
+        # through this lexer
+        lex = bygroups(Token.Marker,
+                        using(lexer.parent),
+                        Token.Marker,
+                        using(lexer.__class__))
+
+        # call it on the matches then iterate through the resulting tokens
+        markers = 0
+        for index, token, value in lex(lexer, match, ctx):
+
+            # keep track of where we are in the line
+            # but don't yield markers in the output stream
+            if token is Token.Marker:
+                markers += 1
+                continue
+
+            # add a Highlight subtype to tokens before the last marker
+            if markers < 2:
+                token = token.Highlight
+
+            # yield the modified stream
+            yield index, token, value
+
+    tokens = {
+        'root': [
+            # find lines where part is enclosed in highlight markers
+            # delgate to add_highlight callback in four match groups:
+            # marker, highlighted content, marker, the rest of the line
+            (rf'({DEFAULT.marker})(.+?)({DEFAULT.marker})(.*)$', add_highlight),
+
+            # all other tokens are inherited from parent class
+            inherit
+        ],
+    }
 
 def make_lexer(lang: str=None, code: str=None, language_lexer: Lexer=None):
-    """Return a HighlightedCodeLexer class subclassed from the language lexer class,
+    """Return a subclass of HighlightedCodeLexer and the language lexer class which is
        either deduced from lang (str) or code (str), or the class of language_lexer
        (pygments.lexer) instance.
 
-    Params
-    ------
-    * language_lexer (pygments.Lexer) -- an instance of the lexer class to subclass
-    * lang (str)                      -- name of the language
-    * code (str)                      -- code to be lexed
+    Params:
+        language_lexer (pygments.Lexer): an instance of the lexer class to subclass
+        lang (str): name of the language
+        code (str): code to be lexed
     """
 
     if not (lang or code or language_lexer):
@@ -59,61 +110,19 @@ def make_lexer(lang: str=None, code: str=None, language_lexer: Lexer=None):
     # get the lexer class to inherit from
     parent = language_lexer.__class__
 
-    class Meta(type(parent)):
-        """Metaclass inheriting from parent metaclasses"""
+    # dynamically define metaclass for __repr__
+    meta = type(
+        f"{parent.__name__.replace('Lexer', '')}Meta",
+        (type(parent),),
+        {"__repr__": __meta_repr__}
+    )
 
-        def __repr__(cls):
-            """A cleaner class repr"""
-            return f"<class {cls.__name__}>"
+    # dynamically define lexer class
+    klass = new_class(
+        f"Highlighted{parent.__name__}",
+        (HighlightedCodeLexer, parent),
+        {"metaclass": meta},
+    )
+    klass.parent = parent
 
-    class HighlightedCodeLexer(parent, metaclass=Meta):
-        """A pygments lexer to that adds a Highlight subtype to all tokens found between
-          the marker text (!!!) and let the parent class lex everything else."""
-
-        def add_highlight(lexer, match, ctx=None):
-            """Add a Highlight subtype to tokens between markers while stripping the
-               markers themselves.
-
-               Expects 4 match groups: marker, highlighted content, marker, rest of line
-            """
-
-            # make a callback function that assigns Token.Marker to the markers, run the
-            # enclosed text through the parent lexer, then run the rest of the line back
-            # through this lexer
-            lex = bygroups(Token.Marker,
-                           using(parent),
-                           Token.Marker,
-                           using(lexer.__class__))
-
-            # call it on the matches then iterate through the resulting tokens
-            markers = 0
-            for index, token, value in lex(lexer, match, ctx):
-
-                # keep track of where we are in the line
-                # but don't yield markers in the output stream
-                if token is Token.Marker:
-                    markers += 1
-                    continue
-
-                # add a Highlight subtype to tokens before the last marker
-                if markers < 2:
-                    token = token.Highlight
-
-                # yield the modified stream
-                yield index, token, value
-
-        tokens = {
-            'root': [
-                # find lines where part is enclosed in highlight markers
-                # delgate to add_highlight callback in four match groups:
-                # marker, highlighted content, marker, the rest of the line
-                (rf'({DEFAULT.marker})(.+?)({DEFAULT.marker})(.*)$', add_highlight),
-
-                # all other tokens are inherited from parent class
-                inherit
-            ],
-        }
-
-    HighlightedCodeLexer.__name__ = f"Highlighted{parent.__name__}"
-
-    return HighlightedCodeLexer
+    return klass
